7389a16f861ae6f8672be21bd059ed1c263734bb
CreaSPallazhco <120573050+CreaSPallazhco@users.noreply.github.com>
2023-08-11 13:01:55 -0500

    Ejemplo5

---
+++ 05-filtrar-list/src/list/ListView.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+// Componente de presenacion
+function ListView({elements, funcFilterItems}) {
+    return (
+        <div>
+            <input type="text" onChange= { ev =>  funcFilterItems(ev.target.value)  } />
+            <ul>
+                {
+                  elements.map((element, index) => { return <li key= { index }> { element } </li> }  )
+                }
+            </ul>
+        </div>
+      );
+}
+
+export default ListView
